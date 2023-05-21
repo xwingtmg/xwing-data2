be038f5404d6bc97ccc58dd5eabc6367f77304c7
Doug Stone-Weaver <doug@stone-weaver.com>
2023-05-21 13:25:47 -0700

    Add plugin to support multiline arrays in prettier, split prettier settings into .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+        // relative paths are usually required so Prettier can find the plugin
+        './node_modules/prettier-plugin-multiline-arrays', // plugin added here
+    ],
+    printWidth: 80,
+    tabWidth: 2,
+};
